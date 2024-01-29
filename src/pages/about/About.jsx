326a456a374c18b3d35939b022a35800f43b34a8
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-01-29 16:29:35 +0330

    feat : Add page About

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+
+function About() {
+  return (
+    <div>
+      <Navbar />
+      <h1>About</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default About;
