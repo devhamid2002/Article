c8c78254be6bedaa84ff2e8f1f0380fe81076769
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-01-29 16:28:03 +0330

    feat : Change Suffix

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
