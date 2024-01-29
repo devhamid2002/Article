72e8e70001196e85cdba238e8dbe978757ed3cb8
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-01-29 16:30:01 +0330

    feat : Add Api call

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,51 +1,25 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 import Article from "../../components/article/Article";
+import axios from "axios";
 
 function Home() {
   //http://localhost:8000/articles
-  let article = [
-    {
-      id: 1,
-      imageUrl:
-        "https://cdn.shopify.com/s/files/1/0373/9041/8051/files/Gaming_Setup_1.jpg?v=1617349925",
-      title: "Noteworthy technology acquisitions 2021",
-      summary:
-        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
-    },
-    {
-      id: 2,
-      imageUrl:
-        "https://cdn.shopify.com/s/files/1/0373/9041/8051/files/Gaming_Setup_1.jpg?v=1617349925",
-      title: "Noteworthy technology acquisitions 2021",
-      summary:
-        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
-    },
-    {
-      id: 3,
-      imageUrl:
-        "https://cdn.shopify.com/s/files/1/0373/9041/8051/files/Gaming_Setup_1.jpg?v=1617349925",
-      title: "Noteworthy technology acquisitions 2021",
-      summary:
-        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
-    },
-    {
-      id: 4,
-      imageUrl:
-        "https://cdn.shopify.com/s/files/1/0373/9041/8051/files/Gaming_Setup_1.jpg?v=1617349925",
-      title: "Noteworthy technology acquisitions 2021",
-      summary:
-        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
-    },
-  ];
+  const [articles, setArticle] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/articles").then((result) => {
+      setArticle(result.data.data);
+    });
+  }, []);
   return (
     <div>
       <Navbar />
       <div className="max-w-[1400px] mx-auto my-12">
         <h1 className="my-14">Article</h1>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 mt-8 mx-auto">
-          {article.map((article) => (
+          {articles.map((article) => (
             <Article key={article.id} article={article} />
           ))}
         </div>
