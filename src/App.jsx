fec9885f167b9aca2403f5c2195f104e7579ba29
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-01-30 20:15:01 +0330

    feat : Add article route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./pages/about/About";
+import Articelpage from "./pages/article page/Articelpage";
 import Home from "./pages/home/Home";
 import { Route, Routes } from "react-router-dom";
 
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="/article/:id" element={<Articelpage />} />
       </Routes>
     </div>
   );
