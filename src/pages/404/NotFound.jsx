5d08addd43b924c74320426e541e0d4ef2fb5c29
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-02-10 17:43:49 +0330

    feat : Add comment

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 function NotFound() {
   return (
     <div>
+      {/* Navbar Camponent */}
       <Navbar />
+      {/* Main 404 */}
       <div class="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
         <div class="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
           <p class="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-gray-300">
@@ -227,6 +229,7 @@ function NotFound() {
           </svg>
         </div>
       </div>
+      {/* Footer Camponent */}
       <Footer />
     </div>
   );
